d67c2a3d425fb1dffec6521922a1894eebc7c2ec
Karine Branco <81647764+KarineMataNada@users.noreply.github.com>
2021-06-12 17:59:34 -0300

    Update EnderecoController.java
    atualização

--- coffee42teste/src/main/java/com/cafeteria/controller/EnderecoController.java
+++ coffee42teste/src/main/java/com/cafeteria/controller/EnderecoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cafeteria.model.Endereco;
 import com.cafeteria.service.EnderecoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-//@CrossOrigin(origins = "*")
-//@Api(value = "API REST Caffee Shop 42")
+
+@CrossOrigin(origins = "*")
+@Api(value = "API REST Caffee Shop 42")
 @RestController
 @RequestMapping("/api/enderecos")
 public class EnderecoController {
@@ -27,31 +31,31 @@ public class EnderecoController {
 	@Autowired
 	private EnderecoService servicoEndereco;
 		
-	//@ApiOperation(value = "Retorna toda lista de Enderecos")
+	@ApiOperation(value = "Retorna toda lista de Enderecos")
 	@GetMapping
 	public List<Endereco> obterTodos() {
 		return servicoEndereco.obterTodos();
 	}
 	
-	//@ApiOperation(value = "Retorna lista de Enderecos por id")
+	@ApiOperation(value = "Retorna lista de Enderecos por id")
 	@GetMapping("/{id}")
 	public Optional<Endereco> obterPorId(@PathVariable("id")Long id){
 		return servicoEndereco.obterPorId(id);
 	}
 	
-	//@ApiOperation(value = "Adiciona Enderecos na lista")
+	@ApiOperation(value = "Adiciona Enderecos na lista")
 	@PostMapping
 	public Endereco adicionar(@RequestBody Endereco Endereco) {		
 		return servicoEndereco.adicionar(Endereco);
 	}
 	
-	//@ApiOperation(value = "Atualiza Endereco na lista por id")
+	@ApiOperation(value = "Atualiza Endereco na lista por id")
 	@PutMapping("/{id}")
 	public Endereco atualizar(@PathVariable(value = "id")Long id, @RequestBody Endereco Endereco){
 		return servicoEndereco.atualizar(Endereco, id);
 	}
 	
-	//@ApiOperation(value = "Deleta Endereco por id")
+	@ApiOperation(value = "Deleta Endereco por id")
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable(value = "id")Long id) {
 		servicoEndereco.deletar(id);
