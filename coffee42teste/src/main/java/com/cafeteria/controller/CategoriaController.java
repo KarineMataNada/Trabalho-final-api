5bb983d6c86054bf530aef0866ea1d619b312e0d
Karine Branco <81647764+KarineMataNada@users.noreply.github.com>
2021-06-13 14:58:39 -0300

    Update CategoriaController.java
    adicionando método por nome e ativando documentação

--- coffee42teste/src/main/java/com/cafeteria/controller/CategoriaController.java
+++ coffee42teste/src/main/java/com/cafeteria/controller/CategoriaController.java
@@ -1,7 +1,6 @@
 package com.cafeteria.controller;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cafeteria.model.Categoria;
 import com.cafeteria.service.CategoriaService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/categorias")
 public class CategoriaController {
@@ -24,31 +25,37 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaService servicoCategoria;
 		
-	//@ApiOperation(value = "Retorna toda lista de Categorias")
+	@ApiOperation(value = "Retorna toda lista de Categorias")
 	@GetMapping
 	public List<Categoria> obterTodos() {
 		return servicoCategoria.obterTodos();
 	}
 	
-	//@ApiOperation(value = "Retorna lista de Categorias por id")
+	@ApiOperation(value = "Retorna lista de Categorias por id")
 	@GetMapping("/{id}")
 	public Optional<Categoria> obterPorId(@PathVariable("id")Long id){
 		return servicoCategoria.obterPorId(id);
 	}
 	
-	//@ApiOperation(value = "Adiciona Categorias na lista")
+	@ApiOperation(value = "Retorna lista de Categorias por nome")
+	@GetMapping("/nome/{nome}")
+	public List<Categoria> obterPorNome(@PathVariable("nome")String nome){
+		return servicoCategoria.obterPorNome(nome);
+	}
+	
+	@ApiOperation(value = "Adiciona Categorias na lista")
 	@PostMapping
 	public Categoria adicionar(@RequestBody Categoria Categoria) {		
 		return servicoCategoria.adicionar(Categoria);
 	}
 	
-	//@ApiOperation(value = "Atualiza Categoria na lista por id")
+	@ApiOperation(value = "Atualiza Categoria na lista por id")
 	@PutMapping("/{id}")
 	public Categoria atualizar(@PathVariable(value = "id")Long id, @RequestBody Categoria Categoria){
 		return servicoCategoria.atualizar(Categoria, id);
 	}
 	
-	//@ApiOperation(value = "Deleta Categoria por id")
+	@ApiOperation(value = "Deleta Categoria por id")
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable(value = "id")Long id) {
 		servicoCategoria.deletar(id);
