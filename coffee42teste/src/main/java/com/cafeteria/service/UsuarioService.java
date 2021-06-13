1bf8d6b4ef753631bc15d04d0acc7de7ad794ffc
Karine Branco <81647764+KarineMataNada@users.noreply.github.com>
2021-06-13 14:13:17 -0300

    Update UsuarioService.java
    Implementando metodo logar

--- coffee42teste/src/main/java/com/cafeteria/service/UsuarioService.java
+++ coffee42teste/src/main/java/com/cafeteria/service/UsuarioService.java
@@ -1,14 +1,22 @@
 package com.cafeteria.service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.cafeteria.dto.LoginResponse;
 import com.cafeteria.exception.ResourceNotFoundException;
 import com.cafeteria.model.Usuario;
 import com.cafeteria.repository.UsuarioRepository;
+import com.cafeteria.security.JWTService;
 
 
 
@@ -16,9 +24,19 @@ import com.cafeteria.repository.UsuarioRepository;
 public class UsuarioService {
 	
 
+	private static final String headerPrefix = "Coffee ";
 
 	@Autowired
 	private UsuarioRepository repositorioUsuario;
+	
+	@Autowired
+	private JWTService jwtService;
+	
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEnconder;
 	
 
 	public List<Usuario> obterTodos() {
@@ -51,13 +69,13 @@ public class UsuarioService {
 	public Usuario adicionar(Usuario usuario) {
 		usuario.setId(null);
 		
-//		if(repositorioUsuario.findByUsername(usuario.getUsername()).isPresent()) {
-//			
-//		}
+		if(repositorioUsuario.findByUsername(usuario.getUsername()).isPresent()) {
+			
+	}
 		
-//		String senha = passwordEnconder.encode(usuario.getSenha());
-//		usuario.setSenha(senha);
-//		
+		String senha = passwordEnconder.encode(usuario.getSenha());
+		usuario.setSenha(senha);
+		
 		return repositorioUsuario.save(usuario);
 		
 	}
@@ -84,19 +102,20 @@ public class UsuarioService {
 		repositorioUsuario.deleteById(id);	 
 }
 	
-//	public LoginResponse logar(String username, String senha) {
-//		
-//		Authentication autenticacao = authenticationManager.authenticate(
-//				new UsernamePasswordAuthenticationToken(username, senha, Collections.emptyList()));
-//		
-//		SecurityContextHolder.getContext().setAuthentication(autenticacao);
-//		
-//		String token = headerPrefix + jwtService.gerarToken(autenticacao);
-//		
-//		var usuario = repositorioUsuario.findByUsername(username);
-//		
-//		return new LoginResponse(token, usuario.get());
-//	}
-//	
-
+	public LoginResponse logar(String username, String senha) {
+		
+		Authentication autenticacao = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(username, senha, Collections.emptyList()));
+		
+		SecurityContextHolder.getContext().setAuthentication(autenticacao);
+		
+		String token = headerPrefix + jwtService.gerarToken(autenticacao);
+		
+		var usuario = repositorioUsuario.findByUsername(username);
+		
+		return new LoginResponse(token, usuario.get());
+	}
+	
 }
+
+
