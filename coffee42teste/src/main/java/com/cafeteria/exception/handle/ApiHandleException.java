267249daa3f61faff438057b95793e7ee07c522b
Karine Branco <81647764+KarineMataNada@users.noreply.github.com>
2021-06-13 17:11:58 -0300

    Update ApiHandleException.java
    atualização dos erros

--- coffee42teste/src/main/java/com/cafeteria/exception/handle/ApiHandleException.java
+++ coffee42teste/src/main/java/com/cafeteria/exception/handle/ApiHandleException.java
@@ -7,8 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.cafeteria.exception.ResourceNotFoundException;
-import com.cafeteria.exception.error.ErrorMessage;
+import com.coffeeshop42.exception.ResourceBadRequestException;
+import com.coffeeshop42.exception.ResourceNotFoundException;
+import com.coffeeshop42.exception.error.ErrorMessage;
 
 
 @ControllerAdvice
@@ -26,4 +27,43 @@ public class ApiHandleException {
 		return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
 		
 	}
+	
+	@ExceptionHandler(ResourceBadRequestException.class)
+	public ResponseEntity<?> handlerResourceBadRequestException(ResourceBadRequestException exception){
+		ErrorMessage errorMessage = new ErrorMessage(
+				"Bad Request",
+				HttpStatus.BAD_REQUEST.value(),
+				exception.getMessage(),
+				exception.getClass().getName(),
+				new Date().getTime());
+		
+		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handlerInternalServerError(Exception exception){
+		ErrorMessage errorMessage = new ErrorMessage(
+				"Internal Server Error",
+				HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				exception.getMessage(),
+				exception.getClass().getName(),
+				new Date().getTime());
+		
+		return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handle(Exception exception){
+		ErrorMessage errorMessage = new ErrorMessage(
+				"403 Forbidden",
+				HttpStatus.FORBIDDEN.value(),
+				exception.getMessage(),
+				exception.getClass().getName(),
+				new Date().getTime());
+		
+		return new ResponseEntity<>(errorMessage, HttpStatus.FORBIDDEN);
+	}
+
+	
 }
