f39a4c2f70c43c4ece155e378eb0e2408a3c387f
Karine Branco <81647764+KarineMataNada@users.noreply.github.com>
2021-06-13 14:10:43 -0300

    Update UsuarioController.java
    atualizando cliente service com o metodo logar

--- coffee42teste/src/main/java/com/cafeteria/controller/UsuarioController.java
+++ coffee42teste/src/main/java/com/cafeteria/controller/UsuarioController.java
@@ -1,71 +1,120 @@
 package com.cafeteria.controller;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.cafeteria.dto.LoginResponse;
+import com.cafeteria.exception.ResourceNotFoundException;
 import com.cafeteria.model.Usuario;
-import com.cafeteria.service.UsuarioService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import com.cafeteria.repository.UsuarioRepository;
+import com.cafeteria.security.JWTService;
 
 
-@CrossOrigin(origins = "*")
-@Api(value = "API REST Caffee Shop 42")
-@RestController
-@RequestMapping("/api/usuarios")
-public class UsuarioController {
 
+@Service
+public class UsuarioService {
+	
+
+	private static final String headerPrefix = "Coffee ";
+
+	@Autowired
+	private UsuarioRepository repositorioUsuario;
 	
 	@Autowired
-	private UsuarioService servicoUsuario;
-		
-	@ApiOperation(value = "Retorna toda lista de Usuarios")
-	@GetMapping
+	private JWTService jwtService;
+	
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEnconder;
+	
+
 	public List<Usuario> obterTodos() {
-		return servicoUsuario.obterTodos();
+		return repositorioUsuario.findAll();
 	}
 	
-	@ApiOperation(value = "Retorna lista de Usuarios por id")
-	@GetMapping("/{id}")
-	public Optional<Usuario> obterPorId(@PathVariable("id")Long id){
-		return servicoUsuario.obterPorId(id);
+	
+	public Optional<Usuario> obterPorId(Long id) {
+		 Optional<Usuario> usuario = repositorioUsuario.findById(id);	 
+		
+		 if(usuario.isEmpty()) {
+			throw new ResourceNotFoundException("Usuario não encontrado!");
+		}
+		 
+		return usuario;
 	}
 	
-	@ApiOperation(value = "Retorna lista de Usuarios por nome")
-	@GetMapping("/nome/{nome}")
-	public List<Usuario> obterPorNome(@PathVariable("nome")String nome){
-		return servicoUsuario.obterPorNome(nome);
+	
+
+	public List<Usuario> obterPorNome(String nome) {
+		List<Usuario> usuario = repositorioUsuario.findByNomeContaining(nome);	
+		
+		if(usuario.isEmpty()) {
+			throw new ResourceNotFoundException("Usuario não encontrado!");
+		}
+		
+		return usuario;
 	}
 	
-	@ApiOperation(value = "Adiciona usuarios na lista")
-	@PostMapping
-	public Usuario adicionar(@RequestBody Usuario usuario) {		
-		return servicoUsuario.adicionar(usuario);
-	}
-	
-	@ApiOperation(value = "Atualiza usuario na lista por id")
-	@PutMapping("/{id}")
-	public Usuario atualizar(@PathVariable(value = "id")Long id, @RequestBody Usuario usuario){
-		return servicoUsuario.atualizar(usuario, id);
-	}
-	
-	@ApiOperation(value = "Deleta usuario por id")
-	@DeleteMapping("/{id}")
-	public void deletar(@PathVariable(value = "id")Long id) {
-		servicoUsuario.deletar(id);
+	public Usuario adicionar(Usuario usuario) {
+		usuario.setId(null);
+		
+		if(repositorioUsuario.findByUsername(usuario.getUsername()).isPresent()) {
+			
 	}
 		
+		String senha = passwordEnconder.encode(usuario.getSenha());
+		usuario.setSenha(senha);
+		
+		return repositorioUsuario.save(usuario);
+		
+	}
+	
+	 public Usuario atualizar(Usuario usuario, Long id) {
+		 Optional<Usuario> usuarioAtualizado = repositorioUsuario.findById(id);
+		 
+		if(usuarioAtualizado.isEmpty()) {
+			throw new ResourceNotFoundException("Usuario não encontrado por id");
+		}
+		usuario.setId(id);		
+		return repositorioUsuario.save(usuario);
+		
+	}
+
+
+	public void deletar(Long id) {
+	    Optional<Usuario> deletarUsuario = repositorioUsuario.findById(id);
+		
+	    if(deletarUsuario.isEmpty()) {
+			throw new ResourceNotFoundException("Usuario não encontrado por id");
+		}
+	    
+		repositorioUsuario.deleteById(id);	 
+}
+	
+	public LoginResponse logar(String username, String senha) {
+		
+		Authentication autenticacao = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(username, senha, Collections.emptyList()));
+		
+		SecurityContextHolder.getContext().setAuthentication(autenticacao);
+		
+		String token = headerPrefix + jwtService.gerarToken(autenticacao);
+		
+		var usuario = repositorioUsuario.findByUsername(username);
+		
+		return new LoginResponse(token, usuario.get());
+	}
+	
 }
 
