9c09560e5e812af296b20fefa2ee8b03a622047e
Ferigatr <81425184+Ferigatr@users.noreply.github.com>
2021-06-14 00:36:09 -0300

    src email configurado
    o scr foi configurado para enviar email e esta enviando email de confirmação de cadastro

--- coffee42teste/src/main/java/com/cafeteria/service/UsuarioService.java
+++ coffee42teste/src/main/java/com/cafeteria/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.cafeteria.service;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,8 @@ import com.cafeteria.exception.ResourceNotAcceptableException;
 import com.cafeteria.exception.ResourceNotFoundException;
 import com.cafeteria.model.Endereco;
 import com.cafeteria.model.Usuario;
+import com.cafeteria.model.email.Mailler;
+import com.cafeteria.model.email.MensagemEmail;
 import com.cafeteria.repository.UsuarioRepository;
 import com.cafeteria.security.JWTService;
 
@@ -48,6 +51,8 @@ public class UsuarioService {
     @Autowired
     private WebClient webClient;
 	
+	@Autowired
+	Mailler mailler;
 
 	public List<Usuario> obterTodos() {
 		return repositorioUsuario.findAll();
@@ -79,17 +84,42 @@ public class UsuarioService {
 	public Usuario adicionar(Usuario usuario) {
 		usuario.setId(null);
 		
+		
 		if(repositorioUsuario.findByUsername(usuario.getUsername()).isPresent()) {
 			throw new ResourceNotAcceptableException("Usuario já existe");
 	}
+		
+		
 		Endereco endereco = cepService.obterEnderecoPorCep(usuario.getEndereco().getCep());
 		usuario.setEndereco(endereco);
 		String senha = passwordEnconder.encode(usuario.getSenha());
 		usuario.setSenha(senha);
 		
+		
+		//email do cadastro 
+		this.repositorioUsuario.save(usuario);
+		
+		
+		var mensagem = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title></head><body><div style=\"text-align: center;\"><h1 style=\"color:blueviolet;\">%s</h1><p style=\"font-size: 20px;\" ><em>Você foi cadastrado ;)</em></p> <a href=\"http://serratec.org/\" target=\"_blank\"> <img src=\"https://mega.ibxk.com.br/2018/07/27/cachorro-se-cocando-27124201260112.gif?ims=610x\" alt=\"Serratec\"> </a></div></body></html>";
+		mensagem  = String.format(mensagem, usuario.getNome());
+		
+		var funcionario = "Bernard <serratecdev@gmail.com>";
+				
+		var email = new MensagemEmail(
+				"Cadastro realizado com sucesso!", 
+				mensagem,
+				funcionario,
+				Arrays.asList(usuario.getEmail()));
+		
+		mailler.enviar(email);
+		
+	
 		return repositorioUsuario.save(usuario);
 		
+
+
 	}
+	
 	
 	 public Usuario atualizar(Usuario usuario, Long id) {
 		 Optional<Usuario> usuarioAtualizado = repositorioUsuario.findById(id);
@@ -100,7 +130,7 @@ public class UsuarioService {
 		usuario.setId(id);		
 		return repositorioUsuario.save(usuario);
 		
-	}
+	 }
 
 
 	public void deletar(Long id) {
